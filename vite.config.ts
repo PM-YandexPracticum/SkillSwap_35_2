a4e72a531c27d3aabdf21bf164344681f2ec8543
Denis Lisushkin <denislisushkin@yandex.ru>
2025-08-14 23:30:09 -0500

    Normalize line endings

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,24 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { alias } from './aliases';
 
 export default defineConfig({
+  plugins: [react()],
+  css: {
+    modules: {
+      localsConvention: 'camelCase' // Чтобы обращаться как styles.myClass даже если в SCSS дефис
+    },
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/app/styles/variables.scss";` // глобальные переменные/миксины
+      }
+    }
+  },
+  resolve: {
+    alias
+  },
   server: {
     port: 4000,
-    open: true,
-  },
+    open: true
+  }
 });
