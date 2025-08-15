5a00151d6943507b6747558bbfe7fba81fcf5084
Denis Lisushkin <denislisushkin@yandex.ru>
2025-08-15 16:45:20 -0500

    Test Husky + lint-staged

--- src/test.ts
+++ src/test.ts
@@ -1,6 +1,6 @@
 const foo = "double quotes"; // eslint: quotes: single
 console.log(foo)
 a
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-const path = require('path');
+import { createRequire } from 'module';  
+
+const path = require('path');  
