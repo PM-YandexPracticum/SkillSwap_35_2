744c51e0ff5a4d90608ed6f9f192a7e332103dc8
Leonid Nikiforov <leonidnikiforov@MacBook-Air-Leonid.local>
2025-08-17 08:03:23 +0900

    feature(component): add badge UI component with stories and fixed stylelint (ignorance of @use)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { BadgeUI } from './shared/ui/badge/badge';
+
 function App() {
   return (
     <>
       <h1>Skill swap project</h1>
+      <BadgeUI category={'Бизнес и карьера'} title={'Бизнес-план'} />
     </>
   );
 }
