891a44cf04133b0e01d070a41d5ccef17bfccbb5
Denis Lisushkin <denislisushkin@yandex.ru>
2025-08-19 02:26:44 -0500

    fix after pull

--- src/shared/ui/checkbox/checkbox.tsx
+++ src/shared/ui/checkbox/checkbox.tsx
@@ -1,32 +1,35 @@
-import styles from './checkbox.module.scss'
+import styles from './checkbox.module.scss';
 
 export interface CheckboxProps {
-    label?: string;
-    name: string;
-    value: string;
-    checked?: boolean;
-    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
-    variant?: 'tick' | 'minus'
+  label?: string;
+  name: string;
+  value: string;
+  checked?: boolean;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  variant?: 'tick' | 'minus';
 }
 
 export const Checkbox: React.FC<CheckboxProps> = ({
-   label, name, value, checked, onChange, variant = 'tick'
-}: CheckboxProps) => {
-  return (
-    <div className={styles.checkbox}>
-        <label className={styles.checkboxLabel}>
-          <input
-          className={styles.checkboxInput}
-            type='checkbox'
-            name={name}
-            value={value}
-            checked={checked}
-            onChange={onChange}
-            data-variant={variant}
-          />
-          <span className={styles.checkmark}></span>
-        {label}
-        </label>
-    </div>
-  );
-};
+  label,
+  name,
+  value,
+  checked,
+  onChange,
+  variant = 'tick'
+}: CheckboxProps) => (
+  <div className={styles.checkbox}>
+    <label className={styles.checkboxLabel}>
+      <input
+        className={styles.checkboxInput}
+        type='checkbox'
+        name={name}
+        value={value}
+        checked={checked}
+        onChange={onChange}
+        data-variant={variant}
+      />
+      <span className={styles.checkmark} />
+      {label}
+    </label>
+  </div>
+);
