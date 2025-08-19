77600e257c4cfdd745ff192f5d0992756c901d91
Leonid Nikiforov <leonidnikiforov@MacBook-Air-Leonid.local>
2025-08-19 22:00:33 +0900

    refactor: add index.ts in shared folder for UI

---
+++ src/shared/index.ts
@@ -0,0 +1,5 @@
+export { TitleUI } from './ui/title';
+export { LikeButtonUI } from './ui/likeButton';
+export { Input } from './ui/input';
+export { Checkbox } from './ui/checkbox';
+export { BadgeUI } from './ui/badge';
