15921867dfd389c2f07a1f7289363f43e54312a6
Ch1lly <JuliJuliaE@yandex.ru>
2025-08-23 13:45:28 +0300

    feature: logo

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client';
 import './app/styles/globals.scss';
 import App from './App.tsx';
 import '../src/app/styles/globals.scss';
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
