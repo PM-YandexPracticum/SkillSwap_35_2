4d08230102d56db540e96583984808fbb2ba8032
Denis Lisushkin <denislisushkin@yandex.ru>
2025-08-24 04:22:12 -0500

    fix: add icon props to input component

--- src/shared/ui/input/input.tsx
+++ src/shared/ui/input/input.tsx
@@ -1,40 +1,55 @@
-// import React from 'react';
 import clsx from 'clsx';
 import styles from './input.module.scss';
-import type { inputProps } from './inputTypes';
+import type { InputProps } from './inputTypes';
 
 export function Input({
   className,
+  inputClassName,
   label,
   type = 'text',
   placeholder,
   value,
   onChange,
-  error
-}: inputProps) {
+  error,
+  icon,
+  iconStyleOverride,
+  inputPadding
+}: InputProps) {
   return (
     <div className={clsx(styles.input, className)}>
       {label && <label className={styles.label}>{label}</label>}
 
-      <input
-        className={clsx(styles.field, error && styles.fieldError)}
-        type={type}
-        placeholder={placeholder}
-        value={value}
-        onChange={onChange}
-      />
+      <div className={styles.fieldWrapper}>
+        {icon && (
+          <span className={styles.icon} style={iconStyleOverride}>
+            {icon}
+          </span>
+        )}
+        <input
+          className={clsx(
+            styles.field,
+            error && styles.fieldError,
+            inputClassName
+          )}
+          type={type}
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}
+          style={icon ? inputPadding : undefined}
+        />
+      </div>
 
       {error && <span className={styles.error}>{error}</span>}
     </div>
   );
 }
 
-export const InputText = (props: inputProps) => (
+export const InputText = (props: InputProps) => (
   <Input {...props} type='text' />
 );
-export const InputEmail = (props: inputProps) => (
+export const InputEmail = (props: InputProps) => (
   <Input {...props} type='email' />
 );
-export const InputPassword = (props: inputProps) => (
+export const InputPassword = (props: InputProps) => (
   <Input {...props} type='password' />
 );
