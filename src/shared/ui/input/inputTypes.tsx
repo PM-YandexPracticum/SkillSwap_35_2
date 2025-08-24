b0cd795e941feba535f8a9525374ae9bf96ea3b4
Denis Lisushkin <162559351+Denis-Lisushkin@users.noreply.github.com>
2025-08-24 12:37:06 -0500

    Fix/add input icon (#78)
    * fix after pull
    
    * fix after pull
    
    * fix: add icon props to input component

--- src/shared/ui/input/inputTypes.tsx
+++ src/shared/ui/input/inputTypes.tsx
@@ -1,11 +1,15 @@
 import type { ChangeEvent } from 'react';
 
-export interface inputProps {
+export interface InputProps {
   className?: string;
+  inputClassName?: string; // дополнительный класс для тега input
   label?: string; // название поля input сверху
   type?: string; // тип поля (password, email и др)
   placeholder?: string; // подсказка в поле
   value?: string;
   onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
   error?: string;
+  icon?: React.ReactNode; // иконка внутри input слева или справа
+  iconStyleOverride?: React.CSSProperties; // позиционирование иконки iconStyleOverride={{ left: '12px' }}
+  inputPadding?: React.CSSProperties; // отступы для input, чтобы текст не налезал на иконку inputPadding={{ paddingLeft: '44px' }
 }
