23bb86f4a6554d20bf07331368662c439433b724
Nikolay <Nikolay@mail.ru>
2025-08-26 09:51:31 +0300

    feature/94-title-add-style

--- src/shared/ui/title/title.stories.tsx
+++ src/shared/ui/title/title.stories.tsx
@@ -13,27 +13,35 @@ type Story = StoryObj<typeof meta>;
 export const TitleH1: Story = {
   args: {
     size: 'h1',
-    text: 'Загловок H1'
+    text: 'Заголовок H1'
   }
 };
 
 export const TitleH2: Story = {
   args: {
     size: 'h2',
-    text: 'Загловок H2'
+    text: 'Заголовок H2'
   }
 };
 
 export const TitleH3: Story = {
   args: {
     size: 'h3',
-    text: 'Загловок H3'
+    text: 'Заголовок H3'
   }
 };
 
 export const TitleH4: Story = {
   args: {
     size: 'h4',
-    text: 'Загловок H4'
+    text: 'Заголовок H4'
+  }
+};
+
+export const TitleWithCustomStyle: Story = {
+  args: {
+    size: 'h1',
+    text: 'Заголовок с кастомным стилем',
+    className: 'underline'
   }
 };
--- src/shared/ui/title/title.tsx
+++ src/shared/ui/title/title.tsx
@@ -1,12 +1,18 @@
 import styles from './title.module.scss';
 import type { THeadingProps, TitleUIProps } from './type';
 
-const Heading = ({ headingSize: Tag = 'h1', text }: TitleUIProps) => (
-  <Tag className={`${styles.title} ${styles['title_' + String(Tag)]}`}>
+const Heading = ({
+  headingSize: Tag = 'h1',
+  text,
+  className
+}: TitleUIProps) => (
+  <Tag
+    className={`${styles.title} ${styles['title_' + String(Tag)]} ${className}`}
+  >
     {text}
   </Tag>
 );
 
-export const TitleUI = ({ size, text }: THeadingProps) => (
-  <Heading headingSize={size} text={text} />
+export const TitleUI = ({ size, text, className }: THeadingProps) => (
+  <Heading headingSize={size} text={text} className={className} />
 );
