2e82a453e61bf48c9cfd5e3d760dbf75be1a2886
Aysylu Tuityrova <ais-94@yandex.ru>
2025-08-27 17:23:06 +0300

    after correction styles

--- src/pages/NotFound404/Page404.tsx
+++ src/pages/NotFound404/Page404.tsx
@@ -1,9 +1,7 @@
-import { Button } from '../../shared/ui/button/button';
 import { useNavigate } from 'react-router-dom';
-import styles from './Page404.module.scss';
 import image404 from '../../shared/assets/images/error 404.svg';
-//import { Footer } from '@/shared/ui/footer/Footer'; //
-//import { Header } from '@/shared/ui/footer/Header';
+import { Button } from '../../shared/ui/button/button';
+import styles from './Page404.module.scss';
 
 export const NotFound404 = () => {
   const navigate = useNavigate();
@@ -16,38 +14,37 @@ export const NotFound404 = () => {
     console.log('Сообщение об ошибке отправлено');
   };
   return (
-	<div className={styles.page}>
-    <div className={styles.container}>
-      <img className={styles.image} src={image404} alt='404' />
-      <div className={styles.textPage}>
-        <h2 className={styles.massageTitle}>Страница не найдена</h2>
-        <p className={styles.massageText}>
-		К сожалению, эта страница недоступна. 
-		Вернитесь на главную страницу или попробуйте позже
-        </p>
-      </div>
-      <div className={styles.buttonList}>
-        <Button
-          onClick={handleReportError}
-          className={`${styles.buttonLeft} ${styles.button}`}
-          buttonType='secondary'
-          disabled={false}
-          text={'Сообщить об ошибке'}
-        >
-          Сообщить об ошибке
-        </Button>
-        <Button
-          onClick={handleGoToMain}
-          className={`${styles.buttonRight} ${styles.button}`}
-          buttonType='primary'
-          disabled={false}
-          text={'На главную'}
-        >
-          На главную
-        </Button>
+    <div className={styles.page}>
+      <div className={styles.container}>
+        <img className={styles.image} src={image404} alt='404' />
+        <div className={styles.textPage}>
+          <h2 className={styles.massageTitle}>Страница не найдена</h2>
+          <p className={styles.massageText}>
+            К сожалению, эта страница недоступна. Вернитесь на главную страницу
+            или попробуйте позже
+          </p>
+        </div>
+        <div className={styles.buttonList}>
+          <Button
+            onClick={handleReportError}
+            className={`${styles.buttonLeft} ${styles.button}`}
+            buttonType='secondary'
+            disabled={false}
+            text={'Сообщить об ошибке'}
+          >
+            Сообщить об ошибке
+          </Button>
+          <Button
+            onClick={handleGoToMain}
+            className={`${styles.buttonRight} ${styles.button}`}
+            buttonType='primary'
+            disabled={false}
+            text={'На главную'}
+          >
+            На главную
+          </Button>
+        </div>
       </div>
     </div>
-	</div>
-	
   );
 };
