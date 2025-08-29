692cd04824c5af10fbbade2c882c0ec210535d39
Ann Zabolotskikh <ann.zblt@yandex.ru>
2025-08-29 19:54:56 +0300

    fix(modal): fix overlay, add new logic

--- src/shared/ui/modal-overlay/modal-overlay.tsx
+++
@@ -1,6 +0,0 @@
-/* eslint-disable jsx-a11y/click-events-have-key-events */
-import styles from './modal-overlay.module.scss';
-
-export const ModalOverlayUI = ({ onClick }: { onClick: () => void }) => (
-  <div role='presentation' className={styles.overlay} onClick={onClick} />
-);
